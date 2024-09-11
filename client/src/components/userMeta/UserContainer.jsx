9d6d81d92738b7d7b82f8ef1a5cd897910c24b62
abdulrhman <abdulrhmanhegazy2003@gmail.com>
2024-09-11 15:10:57 +0300

    add navigating to user profile when user container is clicked

--- client/src/components/userMeta/UserContainer.jsx
+++ client/src/components/userMeta/UserContainer.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useRegistration } from "../../context/RegistrationContext";
 import FollowButton from "../profile/FollowButton";
 
@@ -5,13 +6,18 @@ export default function UserContainer({ userMeta }) {
   const { user } = useRegistration();
   const ImageSrc = `http://localhost:3001/uploads/`;
 
+  const navigate = useNavigate();
+
   function isOwnAccount(user_id) {
     return user_id == user.id;
   }
   return (
     <>
       {userMeta.map((user_i) => (
-        <div key={user_i.username}>
+        <div
+          key={user_i.username}
+          onClick={() => navigate(`/user/${user_i.username}`)}
+        >
           {/* <img src={ImageSrc + user.image} alt="" /> */}
           <h3>{user_i.username}</h3>
           <h4>{user_i.image}</h4>
